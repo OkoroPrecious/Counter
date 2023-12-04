2a8c253ad3b8298d2c9aee5f37d4023463bbc6df
okoro precious <precious.okoro@cicod.com>
2023-12-04 13:26:59 +0100

    Basic js, hrml and css

---
+++ Js_Project/app.js
@@ -0,0 +1,31 @@
+// write the logic for the html structure
+
+let count = 0;
+
+// Select the buttons and the value of the count
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
